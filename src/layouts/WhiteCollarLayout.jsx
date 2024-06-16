b516c0cd4ed8695ac5c7cbd80a082910e46c66d3
Nathan-88 <onwukaebuka88@gmail.com>
2024-06-16 15:06:25 +0100

    header

--- src/layouts/WhiteCollarLayout.jsx
+++ src/layouts/WhiteCollarLayout.jsx
@@ -12,7 +12,7 @@ function WhiteCollarLayout() {
   return (
       <div className="h-auto min-h-svh flex flex-col relative">  
         {/* <Header color='black' backgroundColor='white'  barColor="black"/> */}
-        <Header backgroundColor= 'rgb(237 233 254)' color="black" barColor="black" />
+        <Header backgroundColor= '#F1F1F1' color="black" barColor="black" />
         <main className="flex-1">
             <Outlet /> {/* This is where the child routes will be rendered */}
         </main>
