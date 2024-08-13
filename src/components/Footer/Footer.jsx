1bb132daea0b9c6ac3f98869ff354d9a7265eb74
Nathan-88 <onwukaebuka88@gmail.com>
2024-08-13 08:33:00 +0100

    removed the instagram icon from the footer and added the link for behance and linkedin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,9 +19,9 @@ const Footer = () => {
         <div className="py-16 bg-[#060606] font-work-sans font-[400] text-sm text-text flex flex-col md:flex-row items-center md:justify-around">
             <div className="flex items-center gap-2">
                 <p className="">Socials |</p>
-                <img src={lin} className="h-4" alt="socials"/>
-                <img src={beh} className="h-4" alt="socials"/>
-                <img src={insta} className="h-4" alt="socials"/>
+                <a target="_blank" href="https://www.linkedin.com/in/precious-oguazu"><img src={lin} className="h-4" alt="socials"/></a>
+                <a target="_blank" href="https://www.behance.net/chidiogpreciou"><img src={beh} className="h-4" alt="socials"/></a>
+                {/* <img src={insta} className="h-4" alt="socials"/> */}
             </div>
             <p className="hidden md:block">Copyright © 2022, <span className="text-primary">Precious</span> | All Right Reserved</p>
             <p className="md:hidden">Copyright © 2022, <span className="text-primary">Precious</span> |</p>
